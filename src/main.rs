c02d783fbe655cedb3d273d2e4ab2ad1bac73041
simoin <lfzeng34@gmail.com>
2021-06-02 16:42:44 +0800

    move app_data out

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,25 @@
+use std::sync::Mutex;
+
+use actix_service::Service;
 use actix_web::{App, HttpServer};
+use actix_web::web::Data;
+use futures::FutureExt;
+
 use magnetite_rs::cache::RssCache;
 use magnetite_rs::middleware::Cache;
 use magnetite_rs::rss_service;
-use std::sync::Mutex;
-use actix_service::Service;
-use futures::FutureExt;
-use actix_web::web::Data;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .data(Mutex::new(RssCache::new()))
-            .wrap_fn(|req, srv| {
-                // if let Some(cache) = req.app_data::<Data<Mutex<RssCache>>>() {
-                //     println!("cache len: {}", cache.lock().unwrap().channel.len());
-                // }
-                // let cache = req.app_data::<Mutex<RssCache>>().unwrap().to_owned();
+    let rss_cache = Data::new(Mutex::new(RssCache::new()));
 
-                srv.call(req).map(|res| {
-                    res
-                })
-            })
+    HttpServer::new(move || {
+        App::new()
+            .app_data(rss_cache.clone())
             .wrap(Cache)
             .service(rss_service())
     })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
